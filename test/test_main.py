6e9bd2f0086965c3b03d663d901d8788299ad0aa
sunny <sunnykushwaha789@gmail.com>
2024-09-21 12:29:58 +0530

    all

---
+++ test/test_main.py
@@ -0,0 +1,22 @@
+
+'''import os
+from pathlib import Path
+import sys
+path=Path(os.path.abspath(os.path.dirname(__file__))).parent.parent
+sys.path.append(str(path))'''
+
+from Example_2.src.math_operation import add,sub
+print("example is imported")
+def test_add():
+    assert add(2,3)==5
+    assert add(1,2)==3
+
+
+def test_sub():
+
+    assert sub(3,1)==2
+    assert sub(5,2)==3
+
+
+
+
